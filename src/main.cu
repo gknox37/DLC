d4d01035c0c86e87488410db2342c83937aa9cf7
namit2013 <namitgirdhar@gmail.com>
2017-04-28 21:39:16 -0500

    Changed to latest Krishna's serial compression

--- src/main.cu
+++ src/main.cu
@@ -12,7 +12,7 @@
 #include "range.hpp"
 #include "utils.hpp"
 
-#define blockSize 64  // Power of 2 ?
+#define blockSize 128  // Power of 2 ?
 #define BLOCK_SIZE 1024 //Thread Block size
 
 
@@ -22,27 +22,50 @@ __global__ void decompress_kernel(devX, devY, ){
 
 }
 
-
-void initArray(int size , uint8_t **ptrArray1 , uint16_t ** ptrArray2 , bool *isCompressed)
+long getVals(char * t , int base_size)
 {
-    for ( int i =0 ; i < size ; i++)
+    long a ;
+    if(base_size == 16)
+        a = *((int16_t *)t) ;
+    else if(base_size == 32)
+        a = *((int32_t*)t) ;
+    else
+        a = *((long*)t) ;
+    return a ;
+}
+
+
+void initArray(int size , int16_t *isCompressed)
+{
+    int i ;
+    for ( i =0 ; i < size ; i++)
     {
-        ptrArray1[i] = NULL;
-        ptrArray2[i] = NULL;
-        isCompressed[i] = false ;
+        isCompressed[i] = 0 ;
     }
 }
 
 
-void bdCompress(char* c , int len , uint8_t ** ptrArray1 , uint16_t **ptrArray2 , bool* isCompressed , long* baseVals)
+int bdCompress(char* c , int len , char * compressed ,  int16_t * isCompressed , long * baseVals)
 {
-    char* temp;
+    
+    int bytesCopied = 0 ;
     int blkCounter = 0 ;
-    int numBlocks = len/blockSize ;
+    int numBlocks = ((len - 1/blockSize)) + 1 ; // ceiling
     int i ;
+    int size_index ;
+    int size_array[3] ;
+    size_array[0] = 16 ; size_array[1] = 32 ; size_array[2] = 64 ;
     int offset = 0 ;
-    int numPtrs = blockSize/(sizeof(long)) ;
-    long* ptrArray[numPtrs] ;
+    
+    // long *ptrArray[numPtrs] ;
+    if ( offset + blockSize > len)
+    {
+        baseVals[blkCounter] = 0 ;
+        isCompressed[blkCounter] = offset - len ;
+        memcpy(&compressed[bytesCopied] , &c[offset] , len - offset) ;
+        bytesCopied += len - offset ;
+        return bytesCopied ;
+    }
     
     
     while(offset + blockSize <= len) // Don't want to compress if length remaining is less than block
@@ -53,110 +76,310 @@ void bdCompress(char* c , int len , uint8_t ** ptrArray1 , uint16_t **ptrArray2 
         // Get the minimum value as base so unsigned int 8 can be used for deltas
         // If value ranges are more than max value of unsigned int 8 , try to convert to unsigned int.
         // If ranges are more than unsigned int , don't compress this block , move onto next block.
-        
-        for ( i = 0 ; i < numPtrs ; i++)
+	       //	bool used = false ;
+	       //
+        int minBytesUsed = -1 ;
+        int minCompressed ;
+        int minDivision ;
+        long minVal ;
+        for ( size_index = 0 ; size_index < 3 ; size_index++)
         {
-            ptrArray[i] = (long*) (&c[offset + i*sizeof(long)]) ;
-        }
-        bool flag = false ;
-        long minValue ;
-        for ( i =0 ; i<numPtrs ; i++)
-        {
-            if(flag==false)
-            {
-                flag = true ;
-                minValue = *(ptrArray[i]);
-            }
-            else
-            {
-                if(*(ptrArray[i]) < minValue)
-                minValue = (*(ptrArray[i])) ;
-            }
-        }
-        
-        long range = 0 ;
-        flag = false ;
-        for (i =0 ; i<numPtrs ; i++)
-        {
-            if(flag ==false)
-            {
-                range = (*(ptrArray[i])) - minValue ;
-                flag = true ;
-            }
-            else
-            {
-                if((*(ptrArray[i])) - minValue > range)
-                range = *(ptrArray[i]) - minValue ;
-            }
-        }
-        if(range < pow(2 , sizeof(uint8_t) * 8))
-        // compress into uint8
-        {
-            isCompressed[blkCounter] = true ;
-            baseVals[blkCounter] = minValue ;
-            ptrArray1[blkCounter] = (uint8_t*) malloc ( sizeof(uint8_t)*numPtrs) ;
-            for ( i = 0 ; i < numPtrs ; i++)
-            (ptrArray1[blkCounter])[i] = (*(ptrArray[i])) - minValue ;
+            int base_size = size_array[size_index] ;
+            int numPtrs =  (blockSize*8)/(size_array[size_index]) ;
+            long ptrArray [numPtrs] ;
+            char local_storage[blockSize] ;
             
-        }
-        else if (range < pow(2,sizeof(uint16_t)*8))
-        {
-            // compress into uint16
-            isCompressed[blkCounter] = true ;
-            baseVals[blkCounter] = minValue ;
-            ptrArray2[blkCounter] = (uint16_t*)malloc ( sizeof(uint16_t)*numPtrs) ;
             for ( i = 0 ; i < numPtrs ; i++)
-            (ptrArray2[blkCounter])[i] = (*(ptrArray[i])) - minValue ;
+            {
+                ptrArray[i] = getVals((char*)&c[offset + i*(base_size/8)] , base_size) ;
+            }
+            bool flag = false ;
+            long minValue ;
+            for ( i =0 ; i<numPtrs ; i++)
+            {
+                if(flag==false)
+                {
+                    flag = true ;
+                    minValue =  ptrArray[i];
+                }
+                else
+                {
+                    if( ptrArray[i]< minValue)
+                        minValue = ptrArray[i] ;
+                }
+            }
+            
+            long range = 0 ;
+            flag = false ;
+            for (i =0 ; i<numPtrs ; i++)
+            {
+                if(flag ==false)
+                {
+                    range = (ptrArray[i])  - minValue ;
+                    flag = true ;
+                }
+                else
+                {
+                    if((ptrArray[i]) - minValue > range)
+                        range = ptrArray[i] - minValue ;
+                    //  printf("Calc-Range:%ld\n",range) ;
+                }
+            }
+            printf("Final rnage is %ld , Size is %d, Min Val is %d , Num ptrs is %d\n",range , size_array[size_index] , minValue,numPtrs);
+            if((range < pow(2 , sizeof(uint8_t) * 8)))
+                // compress into uint8
+            {
+                if(minBytesUsed == -1 ){
+                    minBytesUsed = 8*numPtrs ;
+                    minCompressed = 8 ;
+                    minDivision = base_size ;
+                    minVal = minValue ;
+                }
+                else if ( 8 * numPtrs < minBytesUsed)
+                {
+                    minBytesUsed = 8 * numPtrs ;
+                    minCompressed = 8 ;
+                    minDivision = base_size ;
+                    minVal = minValue ;
+                    printf("8: Range : %ld , minValue : %ld , min_div:%d , numPtrs:%d\n" , range , minValue,base_size,numPtrs) ;
+                }
+                
+                
+            }
+            else if ((range < pow(2,sizeof(uint16_t)*8)) && (base_size > 16))
+            {
+                if(minBytesUsed == -1 ){
+                    minBytesUsed = 16 *numPtrs ;
+                    minCompressed = 16 ;
+                    minDivision = base_size ;
+                    minVal = minValue ;
+                }
+                else if ( 16 * numPtrs < minBytesUsed)
+                {
+                    minBytesUsed = 16 * numPtrs ;
+                    minCompressed = 16 ;
+                    minDivision = base_size ;
+                    minVal = minValue ;
+                    printf("16 : Range : %ld, minValue : %ld , min_div:%d , numPtrs:%d\n" , range , minValue,base_size,numPtrs) ;
+                }
+                
+                
+                
+                
+            }
+            
+            else if ( (range < pow ( 2 , sizeof(uint32_t)*8)) && (base_size > 32))
+            {
+                if(minBytesUsed == -1 ){
+                    minBytesUsed = 32 *numPtrs ;
+                    minCompressed = 32 ;
+                    minDivision = base_size ;
+                    minVal = minValue ;
+                }
+                else if ( 32 * numPtrs < minBytesUsed)
+                {
+                    minBytesUsed = 32 * numPtrs ;
+                    minCompressed = 32 ;
+                    minDivision = base_size ;
+                    minVal = minValue ;
+                    printf("32: Range : %ld, minValue : %ld, min_div:%d , numPtrs:%d\n" , range , minValue,base_size,numPtrs) ;
+                }
+                
+                
+                
+            }
+            printf("Bytes used so far :%d\n" , minBytesUsed) ;
         }
+        if ( minBytesUsed >= 0 )
+        {
+            int i ;
+            int numPtrs = (blockSize*8/minDivision) ;
+            long  ptrArray[numPtrs] ;
+            int div_off , compressed_off ;
+            for (i =0 ; i < numPtrs ; i++)
+            {
+                
+                ptrArray[i] = getVals(&c[offset + (i*minDivision/8)] , minDivision)  ;
+                div_off = 3 ;
+                if ( minDivision == 16)
+                {
+                    //   ptrArray[i] = (int16_t *)(&c[offset + i*minDivision]) ;
+                    div_off = 1 ;
+                }
+                else  if (minDivision == 32)
+                {
+                    //  ptrArray[i] = (int32_t*) (&c[offset + i*minDivision]) ;
+                    div_off = 2 ;
+                }
+                
+                if ( minCompressed == 32)
+                {
+                    uint32_t a  = ptrArray[i] - minVal ;
+                    memcpy(&compressed[bytesCopied] , &a , sizeof(uint32_t)) ;
+                    compressed_off = 3 ;
+                }
+                else if ( minCompressed == 16)
+                {
+                    uint16_t a  = ptrArray[i] - minVal ;
+                    memcpy(&compressed[bytesCopied] , &a , sizeof(uint16_t)) ;
+                    compressed_off = 2 ;
+                    
+                    
+                }
+                else if ( minCompressed == 8)
+                {
+                    
+                    uint8_t a  = (ptrArray[i]) - minVal ;
+                    memcpy(&compressed[bytesCopied] , &a , sizeof(uint8_t)) ;
+                    compressed_off = 1 ;
+                    
+                }
+                bytesCopied += (minCompressed/8) ;
+                
+                
+            }
+            isCompressed[blkCounter] = 4*div_off + compressed_off ;
+            baseVals[blkCounter] = minVal ;
+        }
+        
+        else
+        {
+            baseVals[blkCounter] = minVal ;
+            isCompressed[blkCounter] = 0 ;
+            memcpy(&compressed[bytesCopied] , &c[offset] , blockSize) ;
+            bytesCopied += blockSize ;
+        }
+        
         
         offset += blockSize ;
         blkCounter ++ ;
+        if ( offset + blockSize > len)
+        {
+            baseVals[blkCounter] = 0 ;
+            isCompressed[blkCounter] = offset - len ;
+            memcpy(&compressed[bytesCopied] , &c[offset] , len - offset) ;
+            bytesCopied += len - offset ;
+            break ;
+        }
+        
     }
+    
+    return bytesCopied ;
+    
 }
 
+int decompress ( char * compressed , char * decompressed , int bytesCopied , long *baseVals , int16_t *isCompressed , int numBlocks)
+{
+    int i = 0 ;
+    int offset_compressed = 0 ;
+    int offset_decompressed = 0 ;
+    for (i = 0 ; i <numBlocks ; i++) // decompress every block
+    {
+        if(isCompressed[i] ==0)
+        {
+            memcpy(&decompressed[offset_decompressed] , &compressed[offset_compressed] , blockSize) ;
+            offset_compressed += blockSize ;
+            offset_decompressed += blockSize ;
+            continue ;
+        }
+        
+        if (isCompressed[i] <0)
+        {
+            int bytes_to_copy = -1* isCompressed[i] ;
+            memcpy(&decompressed[offset_decompressed] , &compressed[offset_compressed] , bytes_to_copy) ;
+            offset_compressed += bytes_to_copy ;
+            offset_decompressed += bytes_to_copy ;
+            break ;
+        }
+        
+        // If code reaches this point then actual compression has taken place
+        int chunk_size ;
+        int compressed_size ;
+        if((isCompressed[i] / 4 ) ==1 )
+            chunk_size = 2  ;
+        else if ((isCompressed[i]/4) ==2)
+            chunk_size = 4 ;
+        else
+            chunk_size = 8 ;
+        if(isCompressed[i] % 4==1)
+            compressed_size = 1 ;
+        else if (isCompressed[i]%4 ==2)
+            compressed_size = 2 ;
+        else
+            compressed_size = 4 ;
+        
+        int numPtrs = blockSize/chunk_size ;
+        int j ;
+        for (j = 0 ; j < numPtrs ; ++j)
+        {
+            long compressed_val = getVals((char *)&compressed[offset_compressed], compressed_size * 8) ;
+            
+            compressed_val += baseVals[i] ;
+            if (chunk_size ==2 )
+            {
+                int16_t num = compressed_val ;
+                memcpy(&decompressed[offset_decompressed] , &num , chunk_size) ;
+            }
+            else if (chunk_size ==4)
+            {
+                int32_t num = compressed_val ;
+                memcpy(&decompressed[offset_decompressed] , &num , chunk_size) ;
+            }
+            else
+            {
+                long num = compressed_val ;
+                memcpy(&decompressed[offset_decompressed] , &num , chunk_size) ;
+            }
+            offset_compressed += compressed_size ;
+            offset_decompressed += chunk_size ;
+        }
+    }
+    
+    return offset_decompressed ;
+}
 
 int main(int argc, char **argv) {
     
     // get start time
     const auto start = now();
     
-    int longArraySize = 16 ;
+    int longArraySize = 512 ;
     long testArray[longArraySize] ;
     int i ,j ;
     for ( i =0 ; i < longArraySize ; i++)
-    testArray[i] = 5*i ;
-    int numBlocks = (longArraySize * sizeof(long))/blockSize ;
-    uint8_t * ptrArray1[numBlocks] ;
-    uint16_t * ptrArray2[numBlocks] ;
-    long baseVals[numBlocks] ;
-    bool isCompressed[numBlocks] ;
-    initArray(numBlocks , ptrArray1 , ptrArray2 , isCompressed) ;
+        testArray[i] = (100*i)    ;
+    int numBlocks = (((longArraySize * sizeof(long))-1)/blockSize) + 1 ; //ceiling
     
-    bdCompress((char*)testArray , longArraySize * sizeof(long) , ptrArray1 , ptrArray2 , isCompressed , baseVals) ;
-    printf( "Numblocks :%d\n" , numBlocks);
-    for ( i = 0; i<numBlocks ; i++)
+    long baseVals[numBlocks] ;
+    int16_t  isCompressed[numBlocks] ;
+    char * compressed = (char*) malloc(100000) ;
+    initArray(numBlocks , isCompressed) ;
+    
+    int bytesCopied = bdCompress((char*)testArray , longArraySize * sizeof(long) ,compressed ,  isCompressed , baseVals) ;
+    printf("Length , Bytes copied  : %d , %d\n" , longArraySize*sizeof(long) , bytesCopied) ;
+    float compression_ratio = (float)(numBlocks*(sizeof(long) + sizeof(int16_t)) + bytesCopied)/(longArraySize * sizeof(long)) ;
+    for ( i = 0 ; i < numBlocks ; i++)
     {
-        printf("Bases : %d ," , baseVals[i]);
+        printf("Base value , compressed info , Ratio : %lu , %d\n , %f\n", baseVals[i] , isCompressed[i] , compression_ratio) ;
     }
     printf("\n") ;
     int numPtrs = blockSize/sizeof(long) ;
-    for ( i =0 ; i < numBlocks ; i++)
+    for ( i = 0 ; i < numBlocks ; i++)
     {
-        for (  j =0 ; j < numPtrs ; j++)
-        printf("Deltas : %d , ", (int)((ptrArray1[i])[j])) ;
+        printf("Base value , compressed info , Ratio : %lu , %d\n , %f\n", baseVals[i] , isCompressed[i] , compression_ratio) ;
     }
-    int bytesAfterCompress = numBlocks * ( numPtrs + sizeof(uint8_t*)  + sizeof(bool) + sizeof(long)) ;
-    int bytesBeforeCompress =  longArraySize * sizeof(long) ;
-    double ratio = ((double)bytesAfterCompress)/((double)bytesBeforeCompress) ;
-    printf("Ratio : %f\n" , ratio);
-    
-    printf("\n");
+    char * decompressed = (char*) malloc(100000) ;
+    int bytes = decompress(compressed , decompressed , bytesCopied , baseVals , isCompressed , numBlocks) ;
+    printf("Bytes after decompression : %d\n" , bytes) ;
+    bool t = (bytes == longArraySize * sizeof(long)) && (strncmp((char*)testArray , decompressed , bytes) ==0) ;
+    if(t)
+        printf("Successful \n") ;
     
     // get end time
     const auto end = now();
     // get elapsed time in milliseconds
     const auto elapsed = std::chrono::duration<double, std::milli>(end - start).count();
-    std::cout << "Compression time = " << elapsed << " milliseconds.";
+    std::cout << "Compression & Decompression time = " << elapsed << " milliseconds.";
     
     // Transfer compacted to GPU
     // ----------------------------------------
