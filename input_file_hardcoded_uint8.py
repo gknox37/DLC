4924cb3c891196d1d7fd73b1d21edec55607468b
Namit <namg@wirelessprv-10-193-110-221.near.illinois.edu>
2017-05-12 15:23:39 -0500

    Rai changed to execute 3 files, Py for hardcoding input to uint8

---
+++ input_file_hardcoded_uint8.py
@@ -0,0 +1,10 @@
+import sys
+num_elements = int(sys.argv[1])
+f = open('data/input_' + str(num_elements) +'.raw','w')
+f.write(str(num_elements)+'\n')
+for i in range (0 , num_elements):
+        if((i/1000)%2==0):
+		f.write(str((100*(i%31))%256) + '\n')
+        else:
+		f.write(str((10*(i%31))%256) + '\n')
+f.close()
